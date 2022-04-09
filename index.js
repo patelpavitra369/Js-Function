65d5ae49f9d5b6bb48fedc25e02f3cb652d96166
patelpavitra369 <patelpavitra369@gmail.com>
2022-04-09 11:51:16 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function calculation(a, b, type) {
+  if (type == 'sum') {
+    return a + b;
+  } else if (type == 'sub') {
+    return a - b;
+  } else if (type == 'mul') {
+    return a * b;
+  }
+}
+console.log(calculation(1, 2, 'sum'));
+console.log(calculation(1, 2, 'sub'));
+console.log(calculation(1, 2, 'mul'));
